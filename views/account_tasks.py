0cb14b6b49940c16c647864bd83376f2558a31af
Carlos Perez <carlos@searchkings.ca>
2017-08-16 09:08:08 -0400

    Updated current callable tasks so it uses token authentication, instead of basic.

--- views/account_tasks.py
+++ views/account_tasks.py
@@ -1,19 +1,19 @@
 from flask import Blueprint
 from flask import jsonify
-from eve.auth import requires_auth
+from flask_sentinel import oauth
 from app.tasks.accounts import retrieve_new_accounts
 
 task = Blueprint('task', __name__)
 
 @task.route('/new_accounts', methods=['GET'])
-@requires_auth('RolesAuth')
+@oauth.require_oauth()
 def retrieve_new_accounts_trigger():
     task = retrieve_new_accounts.apply_async()
     return jsonify({'task_id': task.id})
 
 
 @task.route('/new_accounts/<task_id>')
-@requires_auth('RolesAuth')
+@oauth.require_oauth()
 def retrieve_new_accounts_status(task_id):
     task = retrieve_new_accounts.AsyncResult(task_id)
     response = {
@@ -21,4 +21,3 @@ def retrieve_new_accounts_status(task_id):
         'info': str(task.info)
     }
     return jsonify(response)
-
