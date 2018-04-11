6bb3a86b5df6d43d4ace3dadf64b96c11d641f76
Carlos Perez <carlos@searchkings.ca>
2018-04-11 16:26:46 -0400

    Integrated Sentry for error reporting.

--- app/factory.py
+++ app/factory.py
@@ -8,6 +8,8 @@ from app import config
 from app.utils.tasker import init_celery
 from app.utils.events import prepare_documents_for_import_callback
 from app.views.tasks import task
+from raven.contrib.flask import Sentry
+
 
 def create_app(config=None, environment=None):
     class BearerAuth(BasicAuth):
@@ -41,18 +43,27 @@ def create_app(config=None, environment=None):
                 token = None
             return self.check_auth(token, allowed_roles, resource, method)
 
-    app = Eve(
+    class BlinkerCompatibleEve(Eve):
+        """
+        Workaround for https://github.com/pyeve/eve/issues/1087
+        """
+        def __getattr__(self, name):
+            if name in {"im_self", "im_func"}:
+                raise AttributeError("type object '%s' has no attribute '%s'" %
+                                     (self.__class__.__name__, name))
+            return super(BlinkerCompatibleEve, self).__getattr__(name)
+
+    app = BlinkerCompatibleEve(
         settings=config.settings,
         auth=BearerAuth
     )
 
     ResourceOwnerPasswordCredentials(app)
     app.on_pre_POST += prepare_documents_for_import_callback
-
     app.config.update(config.extra_settings)
     init_celery(app, celery)
-
     app.register_blueprint(task, url_prefix='/tasks')
 
+    sentry = Sentry()
+    sentry.init_app(app)
     return app
-
